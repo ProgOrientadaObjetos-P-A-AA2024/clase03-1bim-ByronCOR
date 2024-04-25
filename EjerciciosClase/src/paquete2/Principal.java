b26c17bad41dbfb0827900fbd74e15ff12f29884
unknown <bdcordova@utpl.edu.ec>
2024-04-25 17:06:09 -0500

    cambios

--- EjerciciosClase/src/paquete2/Principal.java
+++ EjerciciosClase/src/paquete2/Principal.java
@@ -12,11 +12,37 @@ import paquete1.InstitucionEducativa;
  * @author reroes
  */
 public class Principal {
+
     public static void main(String[] args) {
         /*Un objeto de tipo InstitucionEducativa*/
-        
+
         InstitucionEducativa i1 = new InstitucionEducativa();
-        
-        // System.out.printf("%s     ", i1.);
+
+        String nombre = "Simon Bolivar";
+        String tipoInstitucion = "Fiscomisional";
+        int numeroAlumnos = 1256;
+        int numeroDocentes = 89;
+        int numeroSedes = 5;
+
+        i1.establecerNombre(nombre);
+        i1.establecerTipoInstitucion(tipoInstitucion);
+        i1.establecerNumeroAlumnos(numeroAlumnos);
+        i1.establecerNumeroDocentes(numeroDocentes);
+        i1.establecerNumeroSedes(numeroSedes);
+
+        System.out.printf("Institucion Educativa\n\n"
+                + "Nombre de la Institucion:%s\n"
+                + "Tipo de la Institucion:%s\n"
+                + "Numero de Alumnos:%d\n"
+                + "Numero de Docentes:%d\n"
+                + "Numero de Sedes:%d\n",
+                 i1.obtenerNombre(),
+                 i1.obtenerTipoInstitucion(),
+                 i1.obtenerNumeroAlumnos(),
+                 i1.obtenerNumeroDocentes(),
+                 i1.obtenerNumeroSedes());
+    
+
+
     }
 }
