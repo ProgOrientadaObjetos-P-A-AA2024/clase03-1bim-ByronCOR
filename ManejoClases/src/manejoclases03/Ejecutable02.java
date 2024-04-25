13332c098cfb53ee4ea9fd40fe924a795e9ef8b6
unknown <bdcordova@utpl.edu.ec>
2024-04-25 16:32:57 -0500

    cambios

--- ManejoClases/src/manejoclases03/Ejecutable02.java
+++ ManejoClases/src/manejoclases03/Ejecutable02.java
@@ -20,7 +20,15 @@ public class Ejecutable02 {
         h1.establecerNombre("Vaca Ortíz");
         h2.establecerNombre("Militar");
         h3.establecerNombre("Manuel IM");
-        
+        /*
+        Esta imprimiendo el objeto mendiante el uso del metodo obtener. 
+        Al crear los objetos ya vienen con valor prestablecido por lo que va 
+        imprimir ese valor excepto para los objetos que mediante el metodo 
+        establecer se envio como parametro un valor que se le asignara a un 
+        atributo que va a retornar en el metodo obtener  todos los metodos
+        que se localiza en la clase Hospital.java se debe hacer uso de estos
+        metodos ya que los atributos son privados      
+        */
         System.out.printf("%s - %d - %.2f\n", h1.obtenerNombre(),
                 h1.obtenerNumeroCamas(), h1.obtenerPresupuesto());
         /*
